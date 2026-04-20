5be6cb3116e9fe20206cdf7e3b0bd7294d74369a
baye ibrahim <bayeibrahim566@gmail.com>
2026-04-20 14:52:37 +0200

    mise a jour

--- streamlit.py
+++ streamlit.py
@@ -1,12 +1,13 @@
 import streamlit as st
-import requests
+import joblib
+import numpy as np
 
 st.title("🎓 Prédiction du risque d'abandon scolaire")
 
-url = "http://127.0.0.1:5000/predict"
+# Charger le modèle
+model = joblib.load("model_dropout.pkl")
 
-# 🔥 TES 5 FEATURES EXACTES
-
+# Inputs utilisateur
 average_grade = st.number_input("Note moyenne", 0.0, 20.0)
 absenteeism_rate = st.number_input("Taux d'absentéisme", 0.0, 100.0)
 study_time_hours = st.number_input("Temps d'étude (heures)", 0.0, 100.0)
@@ -15,25 +16,18 @@ score_global = st.number_input("Score global", 0.0, 20.0)
 
 if st.button("Prédire"):
 
-    data = {
-        "average_grade": average_grade,
-        "absenteeism_rate": absenteeism_rate,
-        "study_time_hours": study_time_hours,
-        "study_absence_ratio": study_absence_ratio,
-        "score_global": score_global
-    }
+    # Construire le vecteur d'entrée (IMPORTANT: ordre = entraînement modèle)
+    X = np.array([[average_grade,
+                   absenteeism_rate,
+                   study_time_hours,
+                   study_absence_ratio,
+                   score_global]])
 
-    response = requests.post(url, json=data)
+    # Prédiction
+    prediction = model.predict(X)[0]
 
-    st.write("STATUS:", response.status_code)
-    st.write("RESPONSE:", response.text)
-
-    if response.status_code == 200:
-        result = response.json().get("prediction")
-
-        if result == 1:
-            st.error("⚠️ Élève à risque d'abandon")
-        else:
-            st.success("✅ Élève non à risque")
+    # Résultat
+    if prediction == 1:
+        st.error("⚠️ Élève à risque d'abandon")
     else:
-        st.error("Erreur API")
+        st.success("✅ Élève non à risque")
